64a5ee864d9f2113f970083b25b1505292bdaa58
Sangjoon Kwak <sangjoonkwak@Sangjoonui-MacBookAir.local>
2021-08-31 16:41:07 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ fav }) {
+  return <h1>I Love {fav}</h1>;
+}
+
+const foodILike = [
+  {
+    name: "Kimchi", 
+    image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png"
+  },
+  {
+    name: "Kimbab",
+    image: "https://www.tsunagulocal.com/wp-content/uploads/2021/02/pixta_65991015_M.jpg"
+  },
+  {
+    name: "Pizza",
+    image: "https://media-cdn.tripadvisor.com/media/photo-s/15/9c/11/a4/pizza.jpg"
+  },
+  {
+    name: "fish",
+    image: "https://articlewatt.com/wp-content/uploads/2021/05/Scup-porgy.jpg"
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!</h1>
+      {foodILike.map(dish => <Food name={dish.name} />)}
     </div>
   );
 }
